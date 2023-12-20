67d2cbdfaa597357566da9d467f133d934ee3010
le402n22\uer <amy940503@gmail.com>
2023-12-20 17:00:26 +0800

    換個分析方法，讓使用者能指定要使用Pearson correlation還是Spearman correlation

--- 20231220-1.R
+++ 20231220-1.R
@@ -13,3 +13,16 @@ abline(Result)
 
 Data=read.table("Example.txt", header = TRUE)
 colnames(Data)  
+
+colnames(Data)[1] # 列出第一個欄位名稱
+
+colnames(Data)[4] # 列出第四個欄位名稱(依此類推)
+
+Result1 = lm(Data[,"Weight"]~Data[,"Height"])
+summary(Result1)
+
+Result1 = cor.test(Data[,"Weight"], Data[,"Height"], method = "pearson") # Pearson correlation
+Result1
+
+Result2 = cor.test(Data[,"Weight"], Data[,"Height"], method = "spearman") # Spearman correlation
+Result2
