cc1290c96ef046b3fb6cca5d63c87cd7c26eeed1
le402n22\uer <amy940503@gmail.com>
2023-12-20 16:55:03 +0800

    使控制區的參數隨著Server端的狀況而改變

--- 20231220-1.R
+++ 20231220-1.R
@@ -1,7 +1,15 @@
-Data = read.table("Example.txt", header = TRUE) #讀取Example.txt，並存成"Data"這個物件(資料表格式)
+Data = read.table("Example.txt", header = TRUE)
+head(Data, 10)
 
-Color = "red"          #指定顏色
+summary(Data) 
 
-pdf("plot.pdf")      #開啟一個pdf file，路徑為"plot.pdf"
-plot(Data, col = Color) #將圖畫到開啟中的pdf file上去
-dev.off()            #關掉這個pdf file
+Result = lm(Weight~Height, data = Data) #linear regression test
+Result
+
+summary(Result)
+
+plot(Data$Height, Data$Weight, pch = 19)
+abline(Result)
+
+Data=read.table("Example.txt", header = TRUE)
+colnames(Data)  
